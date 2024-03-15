77ad0968ccda6fc41c2e3e6bbfa1c1e5cbf7e5da
Ilya Bondarchuk <cod3provider@gmail.com>
2024-03-15 12:51:40 +0100

    cleaned App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,51 +4,11 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 import ContactForm from './ContactForm/ContactForm';
-// import ContactsList from './ContactsList/ContactsList';
+import ContactsList from './ContactsList/ContactsList';
 import Filter from './Filter/Filter';
 import Container from './shared/Container/Container';
 
 const App = () => {
-  // const [contacts, setContacts] = useState(() => JSON.parse(localStorage.getItem('contacts')) || []);
-  // const [filter, setFilter] = useState('');
-
-  // useEffect(() => {
-  //   localStorage.setItem('contacts', JSON.stringify(contacts));
-  // }, [contacts]);
-
-  // const createContact = ({ name, number }) => {
-  //   const newContact = {
-  //     id: nanoid(),
-  //     name,
-  //     number,
-  //   };
-  //
-  //   const isExist = contacts.find(contact => contact.name.toLowerCase() === name.toLowerCase());
-  //   if (isExist) {
-  //     toast.warn(`${name} is already in contacts.`);
-  //     return false;
-  //   }
-  //
-  //   setContacts(prevState => {
-  //     return [newContact, ...prevState];
-  //   });
-  // };
-
-  // const handleFilterChange = e => {
-  //   const { value } = e.target;
-  //   setFilter(value);
-  // };
-
-  // const getFilteredContacts = () => {
-  //   return contacts.filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase()));
-  // };
-  //
-  // const handleDelete = contactId => {
-  //   setContacts(prevState => prevState.filter(({ id }) => id !== contactId));
-  // };
-  //
-  // const filteredContacts = getFilteredContacts();
-
   return (
     <Container>
       <h1>Phonebook</h1>
@@ -69,7 +29,7 @@ const App = () => {
 
       <h2>Contacts</h2>
       <Filter />
-      {/*<ContactsList />*/}
+      <ContactsList />
     </Container>
   );
 };
