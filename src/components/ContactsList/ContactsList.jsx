3a48cd860077af671926ceabfb10c0a4d10ae5d6
Ilya Bondarchuk <cod3provider@gmail.com>
2024-03-15 12:49:47 +0100

    refactored ContactsList

--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -1,19 +1,46 @@
+import { ReactComponent as Icon } from '../../icons/trash.svg';
+import { useDispatch, useSelector } from 'react-redux';
+import { getContacts, getFilter } from '../../store/selectors';
+import { deleteContact } from '../../store/contactsSlice';
 import s from './ContactList.module.css';
 
-import {ReactComponent as Icon} from '../../icons/trash.svg';
+const ContactsList = () => {
+  const dispatch = useDispatch();
+  const contacts = useSelector(getContacts);
+  console.log(contacts);
+  const filtered = useSelector(getFilter);
 
-const ContactsList = ({ contacts, onDelete }) => {
+  const getFilterContacts = () => {
+    const filterNormalize = filtered.toLowerCase();
+
+    if (!filterNormalize) {
+      return contacts;
+    }
+    return contacts.filter(contact => contact.name.toLowerCase().includes(filterNormalize));
+  };
+
+  const filter = getFilterContacts();
+
   return (
-    <ul className={s.list}>
-      {contacts.map(({ id, name, number }) =>
-        <li className={s.item} key={id}>
-          {name}: {number}
-          <button className={s.button} onClick={() => onDelete(id)}>
-            <Icon className={s.icon} />
-          </button>
-        </li>,
+    <>
+      {contacts.length > 0 && (
+        <ul className={s.list}>
+          {filter.map(({ id, name, number }) =>
+            <li className={s.item} key={id}>
+              <p>
+                {name}:
+              </p>
+              <p>
+                {number}
+              </p>
+              <button className={s.button} onClick={() => dispatch(deleteContact(id))}>
+                <Icon className={s.icon} />
+              </button>
+            </li>,
+          )}
+        </ul>
       )}
-    </ul>
+    </>
   );
 };
 
